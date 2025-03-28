887cbdbb2540901360d22e2c2b1d9712bc6dcafa
richard618512 <1173653694@qq.com>
2025-03-28 18:59:01 +0800

    Add files via upload

---
+++ parallel.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <windows.h>
+#include <stdlib.h>
+
+using namespace std;
+
+const int n=10000;
+int a[n][n],b[n],sum[n];
+
+int main()
+{
+    long long head,tail,freq;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+            a[i][j] = i + j;
+    }
+
+
+    int gap=100;
+    for(int k=100;k<=n;k+=gap){
+        for(int i=0;i<k;i++){
+            sum[i]=0;
+        }
+
+        QueryPerformanceFrequency((LARGE_INTEGER*)&freq);
+        QueryPerformanceCounter((LARGE_INTEGER*)&head);
+       
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < k; j++)
+            {
+                sum[i] += a[j][i] * b[j];
+            }
+        }
+        //?????
+//        for(int j=0;j<k;j++)
+//            for(int i=0;i<k;i++)
+//                sum[i]+=a[j][i]*b[j];
+
+        //??????
+        for(int i=0;i<k;i++)
+            for(int j=0;j<k;j+=2)
+               sum[i]+=a[j][i]*b[j]+a[j+1][i]*b[j+1];
+//        cout<<(tail-head)*1000.0/freq<<endl;
+        QueryPerformanceCounter((LARGE_INTEGER*)&tail);
+        cout<<"n="<<k<<" time:"<<(tail-head)*1000.0/freq<<"ms"<<endl;
+        if(k==1000)gap=1500;
+        if(k==5500)gap=4500;
+
+
+    }
+
+    return 0;
+}
